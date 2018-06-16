dd6924ee80421a01a2440f34a55395d98a85a787
miketheo423 <miketheo423@gmail.com>
2018-06-16 10:51:38 -0600

    Adds working tab bar and skeletons for profile and map components

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,9 @@ import { Text } from 'react-native';
 import { Scene, Router, Actions } from 'react-native-router-flux';
 
 import SpotList from './components/SpotList';
+import SpotMap from './components/SpotMap';
+import Profile from './components/Profile';
+
 
 const tabIcon = ({ selected, title }) => {
   return (
@@ -31,17 +34,17 @@ const RouterComponent = () => {
           </Scene>
           <Scene key="spotMapFlow" title="Map" icon={tabIcon}>
               <Scene
-                key="spotList"
-                component={SpotList}
-                title="Spots"
+                key="spotMap"
+                component={SpotMap}
+                title="Spot Map"
                 initial
               />
           </Scene>
           <Scene key="profileFlow" title="Profile" icon={tabIcon}>
               <Scene
-                key="spotList"
-                component={SpotList}
-                title="Spots"
+                key="profilePage"
+                component={Profile}
+                title="Profile"
                 initial
               />
           </Scene>
